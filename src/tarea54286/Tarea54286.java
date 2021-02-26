3363047feaf67ca5dfa1f1c983d4917d937e32c8
Edson <Edson@DESKTOP-SRA1FII>
2021-02-25 20:47:13 -0600

    mi github

---
+++ src/tarea54286/Tarea54286.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea54286;
+
+/**
+ *
+ * @author Edson
+ */
+public class Tarea54286 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Me llamo Edson y este es mi proyecto de"
+                + "Github");
+    }
+    
+}
